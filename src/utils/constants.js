0e45abe1038bd66527a75afdb3e1826e7a2585cf
suraj kengar <surajkengar1212@gmail.com>
2026-02-14 16:03:55 +0530

    constant.js updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,5 @@
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+  location.hostname === "localhost" ? "http://localhost:7777" : "https://devtinder-backend-z3i4.onrender.com";
+
+
+
